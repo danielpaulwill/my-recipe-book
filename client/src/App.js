fcd1a37fca50dddfd22192c0917f9d2460080408
Daniel Williams <danielpaulwill@gmail.com>
2022-09-11 21:50:31 -0400

    added more pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './Navbar';
+import MainContainer from './MainContainer';
+import SignUp from './SignUp';
+import Login from './Login';
 
 function App() {
+  const login = <Login />
+  const signUp = <SignUp />
+  
+
   return (
     <div>
       <Navbar />
+      <MainContainer />
 
     </div>
   );
---
+++ client/src/MainContainer.js
@@ -0,0 +1,11 @@
+
+
+function MainContainer() {
+  return (
+    <div>
+      <h1>MainContainer</h1>
+    </div>
+  );
+}
+
+export default MainContainer;
